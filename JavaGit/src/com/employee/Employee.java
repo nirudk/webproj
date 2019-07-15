90bf52f5d2ea4cb58851335f6dbf4bc6e84f1f0d
rahul <nirudk27@gmail.com>
2019-07-15 17:02:24 +0530

    setter added in employee

--- JavaGit/src/com/employee/Employee.java
+++ JavaGit/src/com/employee/Employee.java
@@ -4,4 +4,23 @@ public class Employee {
 	private int eid;
 	private String name;
 	private String address;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
 }
